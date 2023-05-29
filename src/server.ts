19fb06f16d1e88cc123d083d4d8c7585a3cc0bc0
Piyush मिश्र <piyush.raj.kit@gmail.com>
2023-05-29 18:47:19 +0530

    Minor Fixes:
    - Renamed index.ts to server.ts
    - Fixed scripts in package.json
    - Modified routes according to rest schema
    - Added response for undefined url for post url request
    - Fixed response code for missing urls
    - Removed unused variables

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,9 @@ app.use(express.json());
 const PORT = process.env.PORT ?? 3000;
 const API_URL = process.env.API_URL ?? '';
 
-app.get(`${API_URL}/get-urls`, getAllUrls);
-app.post(`${API_URL}/short-url`, shortenUrl);
-app.delete(`${API_URL}/delete/:redirectUrl`, deleteUrl);
+app.get(`${API_URL}/urls`, getAllUrls);
+app.post(`${API_URL}/urls`, shortenUrl);
+app.delete(`${API_URL}/urls/:redirectUrl`, deleteUrl);
 app.get(`*`, redirectToFull);
 
 app.listen(PORT, () => {
