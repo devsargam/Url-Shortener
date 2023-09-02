8dc1047a7738797c86787f6ac3b8fc01bded711e
Sargam <sargampoudel100@gmail.com>
2023-09-02 09:08:34 +0530

    :heavy_plus_sign: fix: add morgan

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
+
 import { router } from './routes';
 import { redirectToFull } from './controllers';
 
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
+
 const PORT = process.env.PORT ?? 3000;
 
 app.use('/api/urls', router);
